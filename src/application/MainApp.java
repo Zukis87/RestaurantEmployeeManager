ebcad7b06e5003614cf3bf2130981c52e5fa61a3
Sarusi, Zuriel(zsarusi) <zsarusi@corp.ebay.com>
2016-05-02 10:28:12 +0300

    .

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import employees.Employee;
 import employees.EmployeeFactory;
@@ -190,7 +192,36 @@ public class MainApp extends Application {
 			return null;
 		}
 	}
+	public File getEmployeeFilePath() {
+	    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+	    String filePath = prefs.get("filePath", null);
+	    if (filePath != null) {
+	        return new File(filePath);
+	    } else {
+	        return null;
+	    }
+	}
 
+	/**
+	 * Sets the file path of the currently loaded file. The path is persisted in
+	 * the OS specific registry.
+	 * 
+	 * @param file the file or null to remove the path
+	 */
+	public void setEmployeeFilePath(File file) {
+	    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+	    if (file != null) {
+	        prefs.put("filePath", file.getPath());
+
+	        // Update the stage title.
+	        primaryStage.setTitle("RestaurantEmployeeManagementApp - " + file.getName());
+	    } else {
+	        prefs.remove("filePath");
+
+	        // Update the stage title.
+	        primaryStage.setTitle("AddressApp");
+	    }
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
